a1afb3d3a99b5864ec5beb064a6e1d49b35940b5
pantho0 <panthomashreky6@gmail.com>
2025-01-27 21:46:47 +0600

    password hasher added to model

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+
 const app: Application = express();
 
 app.use(express.json());
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,7 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "./user.interface";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
 const userSchema = new Schema<IUser>(
   {
@@ -50,5 +52,16 @@ const userSchema = new Schema<IUser>(
     timestamps: true,
   },
 );
+
+userSchema.pre("save", async function (next) {
+  const user = this as IUser;
+  user.password = await bcrypt.hash(user.password, Number(config.bcrypt_salt));
+  next();
+});
+
+userSchema.post("save", function (doc, next) {
+  doc.password = "**********";
+  next();
+});
 
 export const User = model<IUser>("User", userSchema);
