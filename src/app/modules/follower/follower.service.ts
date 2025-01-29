04263c33f9b61a9c2619398866f93adb6eda2f9b
pantho0 <panthomashreky6@gmail.com>
2025-01-29 16:30:03 +0600

    follower function slightly updated

--- src/app/modules/follower/follower.service.ts
+++ src/app/modules/follower/follower.service.ts
@@ -1,15 +1,16 @@
 import { User } from "../user/user.model";
 import { IFollower } from "./follower.interface";
+
 import { Follower } from "./follower.model";
 
 const addFollowersIntoDB = async (payload: IFollower) => {
   const result = await Follower.create(payload);
   if (result) {
     await User.findByIdAndUpdate(payload.followedBy, {
-      $push: { followers: payload.following },
+      $push: { following: payload.following },
     });
     await User.findByIdAndUpdate(payload.following, {
-      $push: { followed: payload.followedBy },
+      $push: { followedBy: payload.followedBy },
     });
   }
 
