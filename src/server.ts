fd3dd360252822179c658d0df69a2cbc36a997de
pantho0 <panthomashreky6@gmail.com>
2025-01-30 23:45:41 +0600

    unhandled exception and rejection has been handled via process exit

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
+import { Server } from "http";
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Technify-Today Server is runnning on ${config.port}`);
     });
   } catch (error) {
@@ -15,3 +18,18 @@ async function main() {
 }
 
 main();
+
+process.on("unhandledRejection", () => {
+  console.log("🚫 unhandledRejection detected, shutting down 🚫");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+  console.log("🚫 unhandledRejection detected, shutting down 🚫");
+  process.exit(1);
+});
